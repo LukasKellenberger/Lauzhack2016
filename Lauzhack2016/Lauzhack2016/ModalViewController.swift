7988b98e14b91eee5963f9372748dd937dd81ca8
Lukas Kellenberger <lukas.kellenberger@gmail.com>
2016-11-20 11:02:08 +0100

    Implemented allergies

--- Lauzhack2016/Lauzhack2016/ModalViewController.swift
+++ Lauzhack2016/Lauzhack2016/ModalViewController.swift
@@ -8,27 +8,28 @@
 
 import UIKit
 
+class AllergyCell: UITableViewCell {
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var isAllowedImage: UIImageView!
+}
 
-class ModalViewController: UIViewController {
+class ModalViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var code: String!
     var product: Product!
+    var allergies: Array<String> = []
+    var productAllergies: Dictionary<String, Array<String>>?
+    let settings = UserDefaults.standard
     
     @IBOutlet weak var productName: UINavigationItem!
-    
     @IBOutlet weak var problemText: UILabel!
-
-
     @IBOutlet weak var allergiesHeader: UILabel!
-
-    @IBOutlet weak var alergies: UIStackView!
-    
+    @IBOutlet weak var alergiesTableView: UITableView!
     @IBOutlet weak var ingredientLabel: UILabel!
-    
     @IBOutlet weak var ingredent: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        product = Product(barcode: code, on_load: {
+        product = Product(barcode: "80135463", on_load: {
             if !self.product.loaded {
                 DispatchQueue.main.sync {
                     self.renderFail()
@@ -43,11 +44,34 @@ class ModalViewController: UIViewController {
                 }
             }
         })
+        alergiesTableView.dataSource = self
+        alergiesTableView.delegate = self
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func boolFromSettings(key: String, defaultValue: Bool) -> Bool {
+        if(settings.object(forKey: key) != nil) {
+            return settings.bool(forKey: key)
+        } else {
+            return defaultValue
+        }
+    }
+    
+    func getActivatedAllergies() {
+        if(boolFromSettings(key: SettingsViewController.ALL_KEY, defaultValue: SettingsViewController.ALL_DEFAULT)) {
+            allergies = Allergens.sharedInstance.allergies
+        } else {
+            for allergy in Allergens.sharedInstance.allergies {
+                let isOn = boolFromSettings(key: allergy, defaultValue: SettingsViewController.DEFAULT)
+                if(isOn) {
+                    allergies.append(allergy)
+                }
+            }
+        }
     }
     
     func renderFail() {
@@ -59,7 +83,10 @@ class ModalViewController: UIViewController {
     }
     
     func renderProduct() {
+        
         if product.hasData() {
+            productAllergies = Allergens.sharedInstance.getProductAllergies(product: product)
+            getActivatedAllergies()
             setProduct()
         } else {
             problemText.text = "We don't have enough information on this product!"
@@ -74,24 +101,21 @@ class ModalViewController: UIViewController {
         
         problemText.isHidden = true
         
-        setAllergies()
+        alergiesTableView.isHidden = false
+        alergiesTableView.reloadData()
         
         ingredientLabel.isHidden = false
         
         ingredent.isHidden = false
         ingredent.lineBreakMode = .byWordWrapping
         ingredent.numberOfLines = 0
-        let alergens: Set<String> = ["Zucker", "Aroma"]
-        setIngredients(ingredients: product.ingredients!, alergens: alergens)
+        setIngredients(ingredients: product.ingredients!, alergens: [])
         
         
     }
     
-    func setAllergies() {
-        
-    }
     
-    func setIngredients(ingredients: Array<String>, alergens: Set<String>) {
+    func setIngredients(ingredients: Array<String>, alergens: Array<String>) {
         let ingredientString = ingredients.joined(separator: ", ")
         let attribute = NSMutableAttributedString.init(string: ingredientString)
         let ingredientNSString = (ingredientString as NSString)
@@ -102,6 +126,38 @@ class ModalViewController: UIViewController {
         ingredent.attributedText = attribute
 
     }
+    
+    func isAllowed(allergy: String) -> Bool {
+        return productAllergies![allergy]!.isEmpty
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allergies.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "allergycell", for: indexPath) as! AllergyCell
+        let name = allergies[indexPath.row]
+        cell.name.text = name
+        cell.imageView?.image = isAllowed(allergy: name) ? #imageLiteral(resourceName: "Yes") : #imageLiteral(resourceName: "No")
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let name = allergies[indexPath.row]
+        setIngredients(ingredients: product.ingredients!, alergens: productAllergies![name]!)
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        setIngredients(ingredients: product.ingredients!, alergens: [])
+    }
+
 
     @IBAction func close(_ sender: Any) {
         self.dismiss(animated: true, completion: nil);
